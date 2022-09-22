4396ce868f2967259a5443be2637440853816e05
Morgan Gicheha <morgangicheha4@gmail.com>
2022-09-22 19:40:15 +0300

    .

--- posts/index.js
+++ posts/index.js
@@ -1,9 +1,28 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const randomstring = require("randomstring");
+
+
 const app = express()
 
+allPosts = {};
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.post('/post',(req, res)=>{
+    const id = randomstring.generate(5);
+    const {title} = req.body
+ 
+    allPosts[id]={
+        id, title
+    }
+
+    res.send(allPosts)
+
+
+})
+
+app.get('/post', (req, res) => {
+  res.send(allPosts)
 })
 
 app.listen(4000, () => {
